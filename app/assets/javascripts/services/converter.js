b8fc4d6c5c222638ab8c171bb7c6f16d840b57af
jrpespisa <jrpespisa@verizon.net>
2016-02-22 17:11:10 -0500

    finished all sequencing logic for RNA and AA

--- app/assets/javascripts/services/converter.js
+++ app/assets/javascripts/services/converter.js
@@ -29,7 +29,19 @@ converterApp.factory('converter', function() {
     var rnaSeq = converter.getRNA(input);
     var rnaArray = splitChars(rnaSeq);
     var map = {
-      aug: "M"
+      aug: "M", acu: "T", aau: "N", agu: "S", guu: "V",
+      auc: "I", acc: "T", aac: "N", agc: "S", guc: "V",
+      auu: "I", aca: "T", aaa: "K", aga: "R", gua: "V",
+      aua: "I", acg: "T", aag: "K", agg: "R", gug: "V",
+      gcu: "A", gau: "D", ggu: "G", uuu: "F", ucu: "S",
+      gcc: "A", gac: "D", ggc: "G", uuc: "F", ucc: "S",
+      gca: "A", gaa: "E", gga: "G", uua: "L", uca: "S",
+      gcg: "A", gag: "E", ggg: "G", uug: "L", ucg: "S",
+      uau: "Y", ugg: "W", cug: "L", ccg: "P", cag: "Q",
+      uac: "Y", cuu: "L", ccu: "P", cau: "H", cgu: "R",
+      ugu: "C", cuc: "L", ccc: "P", cac: "H", cgc: "R",
+      ugc: "C", cua: "L", cca: "P", caa: "Q", cga: "R",
+      cgg: "R", uga: "*", uaa: "*", uag: "*"
     };
     var aaSeqArray = sequenceIterator(rnaArray, aaSeq, map);
     return aaSeqArray.join('')
